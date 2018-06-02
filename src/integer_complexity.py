c2f02f9e38f86007161294290d837bb708f2428f
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-06-03 01:06:48 +0530

    Integer complexity tested for the first million numbers.

--- src/integer_complexity.py
+++ src/integer_complexity.py
@@ -1,4 +1,4 @@
-import math
+import sys
 
 __author__ = "Sudheer"
 
@@ -6,29 +6,22 @@ __author__ = "Sudheer"
 integer_complexity_map = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
 
 
-def get_min_factors(value):
-    root = int(math.sqrt(value))
-    factor1, factor2 = 0, 0
-    for x in range(root, 0, -1):
+def get_factors(value):
+    root = int(value ** 0.5)
+    for x in range(root, 1, -1):
         if value % x == 0:
-            factor1, factor2 = x, value // x
-            break
-    return factor1, factor2
+            yield x, value // x
 
 
 def get_integer_complexity(num):
     if num in integer_complexity_map.keys():
         return integer_complexity_map[num]
     else:
-        factor1, factor2 = get_min_factors(num)
-        if factor1 == num or factor2 == num:
-            integer_complexity_map[num] = get_integer_complexity(num - 1) + 1
+        min_integer_complexity = sys.maxsize
+        for x in get_factors(num):
+            current_integer_complexity = get_integer_complexity(x[0]) + get_integer_complexity(x[1])
+            if current_integer_complexity < min_integer_complexity:
+                min_integer_complexity = current_integer_complexity
         else:
-            integer_complexity_map[num] = min(get_integer_complexity(num - 1) + 1,
-                                              get_integer_complexity(factor1) + get_integer_complexity(factor2))
-
+            integer_complexity_map[num] = min(get_integer_complexity(num - 1) + 1, min_integer_complexity)
         return integer_complexity_map[num]
-
-
-# TODO
-print(get_integer_complexity(92))
