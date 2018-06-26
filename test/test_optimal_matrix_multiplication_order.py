2a018661bf68abf2bbe6410466caf8e1f7716fa3
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-06-26 17:05:27 +0530

    Fixed import statement in test file

--- test/test_optimal_matrix_multiplication_order.py
+++ test/test_optimal_matrix_multiplication_order.py
@@ -1,7 +1,7 @@
-from optimal_matrix_multiplication_order import *
+from src import optimal_matrix_multiplication_order as o
 
 def test_optimal_matrix_multiplication_order():
-    assert 4500 == optimal_matrix_multiplication_operations([10, 30, 5, 60])
-    assert 6000 == optimal_matrix_multiplication_operations([10, 20, 30])
-    assert 26000 == optimal_matrix_multiplication_operations([40, 20, 30, 10, 30])
-    assert 30000 == optimal_matrix_multiplication_operations([10, 20, 30, 40, 30])
+    assert 4500 == o.optimal_matrix_multiplication_operations([10, 30, 5, 60])
+    assert 6000 == o.optimal_matrix_multiplication_operations([10, 20, 30])
+    assert 26000 == o.optimal_matrix_multiplication_operations([40, 20, 30, 10, 30])
+    assert 30000 == o.optimal_matrix_multiplication_operations([10, 20, 30, 40, 30])
