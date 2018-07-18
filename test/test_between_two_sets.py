aafdfbeb1210f44175561668bdb928e3ebde2bfd
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-07-18 13:12:09 +0530

    1. Added tests for between two sets problem. 2. Added problem statement for finding missing numbers and duplicates in sequence. 3. Added max length snake sequence problem statement.

---
+++ test/test_between_two_sets.py
@@ -0,0 +1,8 @@
+__author__ = "Sudheer"
+from src import between_two_sets
+
+def test_between_two_sets():
+    assert 3 == between_two_sets.get_inbetween_factor_count([2, 4], [16, 32, 96])
+    assert 0 == between_two_sets.get_inbetween_factor_count([100, 99, 98, 97, 96, 95, 94, 93, 92, 91], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert 1 == between_two_sets.get_inbetween_factor_count([2], [20, 30, 12])
+    assert 2 == between_two_sets.get_inbetween_factor_count([3, 9, 6], [36, 72])
