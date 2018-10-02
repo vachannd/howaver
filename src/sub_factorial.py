0a307eb5acff0272301e6bcb268faa1d7189f251
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-10-02 17:20:53 +0530

    Added solution and tests for sub factorial problem

--- src/sub_factorial.py
+++ src/sub_factorial.py
@@ -1,4 +1,13 @@
 __author__ = "Sudheer"
 
+import functools
+
+
+@functools.lru_cache(maxsize=3)
 def sub_factorial(n):
-    pass
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return (n - 1) * (sub_factorial(n - 1) + sub_factorial(n - 2))
