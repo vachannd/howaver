cc99fddea4a6fbe5637a8be394319d0729fa1126
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-12-22 12:02:29 +0530

    Brute force solution added to between_two_sets

--- src/between_two_sets.py
+++ src/between_two_sets.py
@@ -1,5 +1,29 @@
 __author__ = "Sudheer"
+import typing
 
 
-def get_inbetween_factor_count(a, b):
-    pass
+def inbetween_number_gen(lower_bound: int, upper_bound: int):
+    for x in range(lower_bound, upper_bound + 1):
+        yield x
+
+
+def get_inbetween_factor_count_brute_force(a: typing.List[int], b: typing.List[int]):
+    """
+    Calculates the number of inbetween factors present between to sets. See <b>between_two_sets<b>
+    problem in problems folder of the repository for the explanation.
+    :param a: List of integers
+    :param b: List of integers
+    :return: Count of inbetween factors.
+    """
+    inbetween_factors = []
+    for x in inbetween_number_gen(a[-1], b[0]):
+        for array_one_ele in a:
+            if x % array_one_ele:
+                break
+        else:
+            for array_two_ele in b:
+                if array_two_ele % x:
+                    break
+            else:
+                inbetween_factors.append(x)
+    return len(inbetween_factors)
--- test/test_between_two_sets.py
+++ test/test_between_two_sets.py
@@ -3,9 +3,9 @@ from src import between_two_sets
 
 
 def test_between_two_sets():
-    assert 3 == between_two_sets.get_inbetween_factor_count([2, 4], [16, 32, 96])
-    assert 0 == between_two_sets.get_inbetween_factor_count(
+    assert 3 == between_two_sets.get_inbetween_factor_count_brute_force([2, 4], [16, 32, 96])
+    assert 0 == between_two_sets.get_inbetween_factor_count_brute_force(
         [100, 99, 98, 97, 96, 95, 94, 93, 92, 91], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     )
-    assert 1 == between_two_sets.get_inbetween_factor_count([2], [20, 30, 12])
-    assert 2 == between_two_sets.get_inbetween_factor_count([3, 9, 6], [36, 72])
+    assert 1 == between_two_sets.get_inbetween_factor_count_brute_force([2], [20, 30, 12])
+    assert 2 == between_two_sets.get_inbetween_factor_count_brute_force([3, 9, 6], [36, 72])
